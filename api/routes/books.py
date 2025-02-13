477aa554583c4d4b520374b53fcb964fed9accd0
unknown <earthbit@protonmail.com>
2025-02-13 02:34:53 +0100

    minor fix

--- api/routes/books.py
+++ api/routes/books.py
@@ -40,7 +40,6 @@ async def create_book(book: Book):
         status_code=status.HTTP_201_CREATED, content=book.model_dump()
     )
 
-
 @router.get(
     "/", response_model=OrderedDict[int, Book], status_code=status.HTTP_200_OK
 )
